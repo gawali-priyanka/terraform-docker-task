154ab598d2161b6c6531c27ed43f526dd2decd60
gawali-priyanka <priyankadeokar1610@gmail.com>
2025-09-25 11:35:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Create a Docker image (pull from Docker Hub)
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+  keep_locally = false
+}
+
+# Create a container from the image
+resource "docker_container" "nginx_container" {
+  name  = "nginx-demo"
+  image = docker_image.nginx.latest
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
 
